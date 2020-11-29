e1913c7acd5765ff85d41fc267202f16ee84c4bd
fedinikita <conifus@yandex.ru>
2020-11-29 16:00:01 +0300

    lesson53

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,14 +4,13 @@ import s from './Users.module.css'
 import userPhoto from '../../assets/images/unnamed.jpg';
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
 
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
             this.props.setUsers(response.data.items)
         })
-        
     }
+
     render() {
         return (
             <div className={s.userItem}>
